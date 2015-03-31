7ee5961c7b3f52e56548833883a77fb0908c5f71
Anton <anton.blomster@gmail.com>
2015-03-31 17:20:03 +0200

    Second push, working button with three random quotes.

--- assignment_1/src/main/java/com/example/anton/assignment_1/MainActivity.java
+++ assignment_1/src/main/java/com/example/anton/assignment_1/MainActivity.java
@@ -1,25 +1,86 @@
 package com.example.anton.assignment_1;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import java.util.Random;
 
 
 public class MainActivity extends ActionBarActivity {
+    TextView quoteText;
+    Button quoteButton;
+    //private String[] quotes = getResources().getStringArray(R.array.quotes);
+
+
+    int[]stringQuotes = {R.string.quote1, R.string.quote2, R.string.quote3};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        quoteText = (TextView) findViewById(R.id.quoteText);
+        quoteButton = (Button) findViewById(R.id.quoteButton);
+
+        quoteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int number = new Random().nextInt(3);
+                String randomString = getString(stringQuotes[number]);
+                Log.i("MainActivity", "Button pressed");
+                quoteText.setText(randomString);
+                //quotes[0]
+            }
+        });
+
+
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("onStop", "Activity stopped");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("onDestroy", "Activity destroyed");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("onPause", "Activity destroyed");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        return false;
     }
 
     @Override
